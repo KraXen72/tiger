8c1d5e85bde79cc99294b07caf7cec12ac8e7140
KraXen72 <dpelech1@gmail.com>
2023-07-29 23:22:15 +0200

    added ruff

--- musicdl.py
+++ musicdl.py
@@ -1,7 +1,6 @@
 import json
 import os
 from os.path import abspath
-from sympy import false
 
 from yt_dlp import YoutubeDL # for downloading the song     DOCS: https://github.com/yt-dlp/yt-dlp
 from tinytag import TinyTag # for checking bitrate          DOCS: https://github.com/devsnd/tinytag
@@ -11,21 +10,21 @@ from eyed3.id3.frames import ImageFrame
 from eyed3.id3 import ID3_V2_3, ID3_V2_4
 import urllib.request # for downloading od thumbnail        DOCS: https://docs.python.org/3/library/urllib.request.html#module-urllib.request
 
-from thumblogic import thumb_gui_crop
-from smart_metadata import smart_metadata
+from thumbnail_gui import thumb_gui_crop
+from metadata import smart_metadata
 
 # you can use https://greasyfork.org/en/scripts/446275-youtube-screenshoter to get any frame of the video quickly (hold ctrl to download instead of clipboard)
 
 # set up urllib. I honestly could've used urllib3 but cba this example was on stackoverflow
 opener = urllib.request.build_opener()
-opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+opener.addheaders = [("User-agent", "Mozilla/5.0")]
 urllib.request.install_opener(opener)
 
 def download_image(url, file_path, file_name):
 	full_path = file_path + file_name
 	urllib.request.urlretrieve(url, full_path)
 
-def sanitizeText(text):
+def sanitize_text(text):
 	forbidden = [ "<", ">", ":", '"', "\\", "/", "|", "?", "*", "."]
 	for letter in forbidden:
 		text = text.replace(letter, "_")
@@ -33,7 +32,7 @@ def sanitizeText(text):
 
 def user_picks_tag(text, default):
 	default_text = "(enter a value)" if default == "" else default
-	assemble = "{}: {} > ".format(text, default_text)
+	assemble = f"{text}: {default_text} > "
 	q = input(assemble)
 	if q.lower() == "" and default_text != "(enter a value)":
 		return default
@@ -44,8 +43,8 @@ def format_release_date(d, current_id3v):
 	if current_id3v == ID3_V2_4:
 		return f"{d['year']}-{d['month']}-{d['day']}"
 	else:
-		return d['year'] # return just the year for id3v2.3
-
+		return d["year"] # return just the year for id3v2.3
+	
 # constants
 ASSET_DIR = "musicdl_assets"
 CWD = abspath(os.getcwd())
@@ -59,15 +58,16 @@ ytd_opts = {
 	#"ffmpeg_location": "D:/coding/yt-dlp/ffmpeg-master-latest-win64-gpl-shared/bin/ffmpeg.exe",
 	"force-overwrites": True,
 	"audio_quality": 0,
-	'format': 'mp3/bestaudio/best',
+	"format": "mp3/bestaudio/best",
 	# ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
-	'postprocessors': [{  # Extract audio using ffmpeg
-		'key': 'FFmpegExtractAudio',
-		'preferredcodec': 'mp3',
+	"postprocessors": [{  # Extract audio using ffmpeg
+	"key": "FFmpegExtractAudio",
+	"preferredcodec": "mp3",
 	}],
 	"test": False
 }
 
+# TODO move this to a file so CTRL + U + ruff doesen't break it
 print("""
 ▄▄▄█████▓  ██▓ ▄████  ▓█████ ██▀███  
 ▓  ██▒ ▓▒▒▓██▒ ██▒ ▀█ ▓█   ▀▓██ ▒ ██▒
@@ -77,33 +77,33 @@ print("""
   ▒ ░░    ░▓  ░▒   ▒ ░░░ ▒░ ░ ▒▓ ░▒▓░
 	░    ░ ▒ ░ ░   ░ ░ ░ ░    ░▒ ░ ▒ 
   ░ ░    ░ ▒ ░ ░   ░     ░    ░░   ░ 
-		   ░       ░ ░   ░     ░     
+           ░       ░ ░   ░     ░     
 """)
 print("Tiger: youtube music downloader for lazy perfectionists.")
 print()
 
-if os.path.exists(abspath(CWD + "/" + "config.json")): 
-	f = open("config.json", 'r', encoding='utf8')
+if os.path.exists(abspath(CWD + "/" + "config.json")):
+	f = open("config.json", "r", encoding="utf8")
 	config_obj = json.load(f)
 	f.close()
 
 	# apply config options
-	if 'savedir' in config_obj:
+	if "savedir" in config_obj:
 		savedir = config_obj["savedir"]
-	if 'save_json_dump' in config_obj:
+	if "save_json_dump" in config_obj:
 		save_json_dump = config_obj["save_json_dump"]
-	if 'skip_reencode' in config_obj:
+	if "skip_reencode" in config_obj:
 		skip_reencode = config_obj["skip_reencode"]
-	if 'ffmpeg_location' in config_obj:
+	if "ffmpeg_location" in config_obj:
 		ytd_opts["ffmpeg_location"] = config_obj["ffmpeg_location"]
 	else:
 		print("[warning]: no ffmpeg location detected in config. falling back to PATH/system")
 
-	if 'id3v' in config_obj:
-		if config_obj['id3v'] == "2.3":
+	if "id3v" in config_obj:
+		if config_obj["id3v"] == "2.3":
 			CURRENT_ID3V = ID3_V2_3
 			print("[info] config set the ID3 version to " + config_obj["id3v"])
-		if config_obj['id3v'] == "2.4":
+		if config_obj["id3v"] == "2.4":
 			CURRENT_ID3V = ID3_V2_4
 			print("[info] config set the ID3 version to " + config_obj["id3v"])
 
@@ -124,11 +124,11 @@ print()
 link = input("Paste link you want to download: ")
 with YoutubeDL(ytd_opts) as ydl:
 	prefetch_info = ydl.extract_info(link, download=False, )
-	if '(Official' in prefetch_info['title'] and ' Video)' in prefetch_info['title']:
+	if "(Official" in prefetch_info["title"] and " Video)" in prefetch_info["title"]:
 		print('[warning] link you entered contains "(Official" and "Video)"')
 		print("It is recommended to use a music.youtube.com url or (Offical Audio) instead.")
 		confirm = input("type Y/y/Yes to continue, anything else to abort: ")
-		if confirm.lower() not in ['y', 'yes']:
+		if confirm.lower() not in ["y", "yes"]:
 			quit()
 print()
 
@@ -143,11 +143,11 @@ if "&t=" in link:
 if "&list=" in link:
 	print("[Download song or playlist?]")
 	opt = input("type 'p' for playlist, anything else (even enter) for song only:")
-	if opt == 'p':
+	if opt == "p":
 		print("[info] downloading whole playlist")
 	else:
 		link = link[:link.index("&list=")] #slice off the index part
-		print('[info] downloading song only')
+		print("[info] downloading song only")
 
 
 with YoutubeDL(ytd_opts) as ydl:
@@ -156,7 +156,7 @@ with YoutubeDL(ytd_opts) as ydl:
 	info = ydl.sanitize_info(info)
 
 	if save_json_dump:
-		f = open(ASSET_DIR + "/" +"jsondump.json", 'w', encoding='utf8')
+		f = open(ASSET_DIR + "/" +"jsondump.json", "w", encoding="utf8")
 		json.dump(info, f, indent=4, ensure_ascii=False)
 		f.close()
 
@@ -173,12 +173,12 @@ with YoutubeDL(ytd_opts) as ydl:
 		if bitrate > desired_bitrate:
 			desired_bitrate = 320.00
 
-		if skip_reencode == False:
-			inpDir, outDir = {}, {}
-			inpDir[filepath] = None
-			outDir[newconcatfn] = f"-hide_banner -c:a libmp3lame -b:a {desired_bitrate}k -y"
+		if skip_reencode is False:
+			inp_dir, out_dir = {}, {}
+			inp_dir[filepath] = None
+			out_dir[newconcatfn] = f"-hide_banner -c:a libmp3lame -b:a {desired_bitrate}k -y"
 
-			ff = FFmpeg(executable=ytd_opts["ffmpeg_location"], inputs=inpDir, outputs = outDir)
+			ff = FFmpeg(executable=ytd_opts["ffmpeg_location"], inputs=inp_dir, outputs = out_dir)
 			# print(ff.cmd)
 			print(f"> re-encoding to {desired_bitrate}k")
 			ff.run(stdout=None)
@@ -189,12 +189,12 @@ with YoutubeDL(ytd_opts) as ydl:
 
 		# 3. SELECT THUMBNAIL
 		# thumb_or_frame = input("> Album Art: Use frame from the video or Thumbnail? type f = frame, anything else (including enter) = thumb")
-		
+
 		thumb_fullpath = ASSET_DIR + "/" + f"thumb[{id}].jpg"
-		if os.path.exists(thumb_fullpath) == False:
+		if os.path.exists(thumb_fullpath) is False:
 			thumb_url = info["thumbnail"].replace("/vi_webp/", "/vi/").replace(".webp", ".jpg")
 			download_image(thumb_url, ASSET_DIR + "/", f"thumb[{id}].jpg")
-		
+
 		# 4. CALL GUI TO CROP THUMBNAIL
 		thumb_fn = f"thumb[{id}].jpg"
 		thumb_path = ASSET_DIR + "/" + thumb_fn
@@ -203,7 +203,7 @@ with YoutubeDL(ytd_opts) as ydl:
 
 		# 5. tag the mp3
 		# youtube has some extra fields for music-type videos, so we try to offer those, but fallback to the next best thing
-		print("Tagging the song:") 
+		print("Tagging the song:")
 		print("You will be asked about some tags, most of them have a default value")
 		print("If it looks good, just hit enter")
 		print("If you type anything else, it will be used as the new value.")
@@ -220,12 +220,20 @@ with YoutubeDL(ytd_opts) as ydl:
 		artist = user_picks_tag("[Artist]", md["artist"])
 		song.tag.artist = artist
 
+		final_filename_path = abspath(savedir + "/" + f"{sanitize_text(artist)} - {sanitize_text(title)}.mp3")
+		if os.path.exists(final_filename_path):
+			print("The songs you're downloading already exists.")
+			print("Continue with tagging & overwrite file? Type A to abort, anything else to continue.")
+			_overwrite = input("A/*: ")
+			if _overwrite == "A":
+				quit()
+
 		album_artist = user_picks_tag("[Album artist]", md["album_artist"])
 		song.tag.album_artist = album_artist
 
 		album = user_picks_tag("[Album]", md["album"])
 		song.tag.album = album
-		
+
 		genre = input("[Genre]: > ")
 		if genre != "":
 			song.tag.genre = genre
@@ -233,24 +241,22 @@ with YoutubeDL(ytd_opts) as ydl:
 		publisher = user_picks_tag("[Publisher]", md["publisher"])
 		if publisher != "":
 			song.tag.publisher = publisher
-		
+
 		# upload date usually corresponds to release date even for music-type videos
-		rel_date = user_picks_tag("[Release Date]", format_release_date(md['year'], CURRENT_ID3V))
+		rel_date = user_picks_tag("[Release Date]", format_release_date(md["year"], CURRENT_ID3V))
 		song.tag.release_date = rel_date # this should set TORY when tag version is id3v2.3
 		song.tag.original_release_date = rel_date
 		song.tag.original_year = rel_date
 		song.tag.recording_date = rel_date # the actual year tag
-		
+
 		song.tag.track_num = user_picks_tag("[Track No #]", (1, 1))
 
 		# add the album cover
 		# God bless https://stackoverflow.com/questions/38510694/how-to-add-album-art-to-mp3-file-using-python-3#39316853
-		song.tag.images.set(ImageFrame.FRONT_COVER, open(ASSET_DIR + "/" + "out.jpg",'rb').read(), 'image/jpeg')
-		
+		song.tag.images.set(ImageFrame.FRONT_COVER, open(ASSET_DIR + "/" + "out.jpg","rb").read(), "image/jpeg")
+
 		song.tag.save(version=CURRENT_ID3V) # save tag to song. id3v2.3 is safer
-		os.rename(filepath, abspath(savedir + "/" + f"{sanitizeText(artist)} - {sanitizeText(title)}.mp3")) # use abspath here to respect savedir
+		os.rename(filepath, final_filename_path) # use abspath here to respect savedir
 
 		print("> Done!")
 		os.system("pause")
-
-
